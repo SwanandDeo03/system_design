7a2ddb8472cd43a76acf4aade65575f5ec87bd7c
Swanand Deo <swananddeo3@gmail.com>
2026-01-17 18:13:42 +0530

    trace.js change

--- tracing.js
+++ tracing.js
@@ -1,24 +1,32 @@
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { Resource } = require('@opentelemetry/resources');
+const { resourceFromAttributes } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
 const serviceName = process.env.OTEL_SERVICE_NAME || 'simple-diary';
 
 const traceExporter = new OTLPTraceExporter({
-  url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT || undefined,
+  url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+});
+
+const resource = resourceFromAttributes({
+  [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
 });
 
 const sdk = new NodeSDK({
-  resource: new Resource({ [SemanticResourceAttributes.SERVICE_NAME]: serviceName }),
+  resource,
   traceExporter,
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
 sdk.start()
-  .then(() => console.log('OpenTelemetry SDK started'))
-  .catch((err) => console.error('Error starting OpenTelemetry SDK', err));
+  .then(() => {
+    console.log('OpenTelemetry SDK started');
+  })
+  .catch((err) => {
+    console.error('Error starting OpenTelemetry SDK', err);
+  });
 
 process.on('SIGTERM', () => {
   sdk.shutdown()
